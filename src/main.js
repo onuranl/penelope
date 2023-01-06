9ac447c7e69e7688140e947feaae6bbc2d1d035f
onuranl <onuro99cent@gmail.com>
2023-01-06 03:19:06 +0300

    Add vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
 
 import './styles/app.css';
 
-createApp(App).mount('#app')
+const store = createStore({
+    state() {
+        return {
+            count: 0
+        }
+    },
+    mutations: {
+        increment(state) {
+            state.count++
+        }
+    }
+})
+
+
+const app = createApp(App)
+
+app.use(store)
+
+app.mount('#app')
