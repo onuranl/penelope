af4bdcec35e70e8b2cfbbef4a6f7f68da4ec7e75
onuranl <onuro99cent@gmail.com>
2023-01-06 13:29:51 +0300

    Complete store structure

--- src/main.js
+++ src/main.js
@@ -4,19 +4,66 @@ import App from './App.vue'
 
 import './styles/app.css';
 
+const { ipcRenderer } = require("electron");
+
 const store = createStore({
     state() {
         return {
-            count: 0
+            baseUrl: "youtube.com/watch?v=",
+            videoIdRegex: /^[a-zA-Z0-9-_]{11}$/,
+            videoID: null,
+            videoDetail: null,
+            selectedType: 'mp3',
+            selectedQuality: 'highestvideo',
+            loading: false,
+            downloading: false,
         }
     },
+    getters: {
+        videoFormatController: (state) => state.videoIdRegex.test(state.videoID),
+        isVideoValid: (state) => state.videoDetail && state.videoDetail === "Video unavailable" ? false : state.videoDetail
+    },
     mutations: {
-        increment(state) {
-            state.count++
+        setVideoID(state, value) {
+            state.videoID = value
+        },
+        setVideoDetail(state, value) {
+            state.videoDetail = value
+        },
+        setSelectedType(state, value) {
+            state.selectedType = value
+        },
+        setSelectedQuality(state, value) {
+            state.selectedQuality = value
+        },
+        setLoading(state, value) {
+            state.loading = value
+        },
+        setDownloading(state, value) {
+            state.downloading = value
+        }
+    },
+    actions: {
+        getDetail({ state, getters, commit }) {
+            if (getters.videoFormatController) {
+                commit('setLoading', true)
+
+                ipcRenderer.send("yt:detail", state.videoID);
+            }
+        },
+        download({ state, commit }) {
+            commit('setDownloading', true)
+
+            ipcRenderer.send(
+                "yt:download", {
+                videoURL: "https://www." + state.baseUrl + state.videoID,
+                type: state.selectedType,
+                quality: state.selectedQuality,
+            }
+            );
         }
     }
 })
-
 
 const app = createApp(App)
 
